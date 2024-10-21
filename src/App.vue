58dc7670570810065f553115826cb72993751a47
KikeChavez1997 <enrique.chavez.r97@gmail.com>
2024-10-21 00:29:57 -0600

    Inicio app vue

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+  
+  import Formulario from './components/Formulario.vue'
+  import useClima from './composables/useClima'
+  import Clima from './components/Clima.vue'
+  import Spinner from './components/Spinner.vue'
+  import Alerta from './components/Alerta.vue'
+
+  const { obtenerClima, clima, mostrarClima, cargando, error } = useClima()
+
+</script>
+
+<template>
+
+    <h1 class="titulo">Buscador de Clima</h1>
+    <div class="contenedor buscador-clima">
+      <Formulario
+          @obtener-clima="obtenerClima">
+
+      </Formulario>
+
+      <Spinner v-if="cargando"></Spinner>
+
+      <Alerta v-if="error">{{ error }}</Alerta>
+
+      <Clima
+      v-if="mostrarClima"
+      :clima="clima"
+      >
+
+      </Clima>
+    </div>
+
+</template>
+
---
+++ src/components/Clima.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+import useClima from '../composables/useClima'
+
+const { formatearTemperatura} = useClima()
+
+
+    defineProps({
+        clima: {
+            type: Object,
+            required: true
+        }
+    })
+
+</script>
+
+<template>
+    <div class="resultado">
+        <h2>Clima de:{{ clima.name }} </h2>
+        <p class="actual">{{ formatearTemperatura(clima.main.temp)  }} &deg;c</p>
+        <div class="temperatura">
+            <p>Min: {{ formatearTemperatura(clima.main.temp_min) }} <span>&deg;c</span></p>
+            <p>Max: {{ formatearTemperatura(clima.main.temp_max) }}<span>&deg;c</span></p>
+        </div>
+
+    </div>
+</template>
